c8f12d08a1d355597fe81f83b37588465d49b026
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2018-06-02 23:58:46 +0430

    initialized

---
+++ .next/4.98553d429808d8d61b2c.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: /home/mohammad/Desktop/FirstNextProject/pages/index.js: Unterminated JSX contents (4:8)\n\n  2 |   <div>\n  3 |     <p>Hello Next.js\n> 4 |   </div>\n    |         ^\n  5 | )\n  6 | \n  7 | export default Index\n    at _class.raise (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:778:15)\n    at _class.jsxReadToken (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:7727:16)\n    at _class.readToken (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:8095:21)\n    at _class.nextToken (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:976:12)\n    at _class.next (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:912:10)\n    at _class.eat (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:917:12)\n    at _class.expect (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:2051:10)\n    at _class.jsxParseClosingElementAt (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:8001:12)\n    at _class.jsxParseElementAt (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:8020:39)\n    at _class.jsxParseElementAt (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:8024:34)\n    at _class.jsxParseElement (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:8077:19)\n    at _class.parseExprAtom (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:8084:21)\n    at _class.parseExprSubscripts (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:2725:21)\n    at _class.parseMaybeUnary (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:2704:21)\n    at _class.parseExprOps (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:2616:21)\n    at _class.parseMaybeConditional (/home/mohammad/Desktop/FirstNextProject/node_modules/babylon/lib/index.js:2588:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=4.98553d429808d8d61b2c.hot-update.js.map
